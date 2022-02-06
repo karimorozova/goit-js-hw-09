d4e1516c76e4a9ed8032480114f85368ab3fbe4b
karine-morozova <morozova_karine@ukr.net>
2022-02-06 15:24:03 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const refs =  {
+    startColorSwitcherBtn: document.querySelector('[data-start]'),
+    stopColorSwitcherBtn: document.querySelector('[data-stop]'),
+};
 
+let setTimeoutId = null;
+
+refs.stopColorSwitcherBtn.disabled = 'true';
+// refs.stopColorSwitcherBtn.setAttribute('disabled', 'true');
+refs.stopColorSwitcherBtn.addEventListener('click', onStopColorSwitcherBtnClick);
+refs.startColorSwitcherBtn.addEventListener('click', onStartColorSwitcherBtnClick);
+
+
+function onStopColorSwitcherBtnClick() {
+    // console.log(refs.stopColorSwitcherBtn);
+    refs.startColorSwitcherBtn.removeAttribute('disabled');
+    refs.stopColorSwitcherBtn.disabled = 'true';
+    // clearTimeout(setTimeoutId)
+};
+
+function onStartColorSwitcherBtnClick() {
+    // console.log(refs.startColorSwitcherBtn);
+    refs.startColorSwitcherBtn.disabled = 'true';
+    refs.stopColorSwitcherBtn.removeAttribute('disabled');
+
+    // setTimeoutId = setTimeout(() => {}, 1000);
+
+};
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
