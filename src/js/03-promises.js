f22907e42c8123ec3903c9cea382c3cacebbdc30
karine-morozova <morozova_karine@ukr.net>
2022-02-07 16:30:37 +0200

    Finish task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,56 @@
+import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+
+
+  const formDataRefs = document.querySelector('.form');
+
+const { elements: { delay, step, amount} } = formDataRefs;
+
+
+formDataRefs.addEventListener('submit', onFormSubmit)
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+ 
+  return new Promise((res, rej) => {
+    if (shouldResolve) {
+      
+      setTimeout(() => {res({ position, delay });}, delay)
+      
+      } else {
+        
+        setTimeout(() => {rej({ position, delay });}, delay)
+        
+      }
+  })
+}
+
+function onFormSubmit(e) {
+
+  e.preventDefault();
+
+  let promisePosition = 0;
+  let promiseDelay = Number(delay.value) - Number(step.value);
+
+
+  for (let i = 0; i < amount.value; i += 1) {
+    
+    promisePosition += 1;
+    promiseDelay += Number(step.value);
+    createPromise(promisePosition, promiseDelay)
+      .then(({ position, delay }) => {
+        
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        
+      })
+      .catch(({ position, delay }) => {
+        
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        
+      });
+    
   }
+
 }
